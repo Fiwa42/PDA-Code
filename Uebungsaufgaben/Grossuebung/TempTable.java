26623360924f978bf47d04b5c8c16900e86f9aef
Fiwa <69937337+Fiwa42@users.noreply.github.com>
2023-11-12 15:54:48 +0100

    Add files via upload

---
+++ Uebungsaufgaben/Grossuebung/TempTable.java
@@ -0,0 +1,14 @@
+public class TempTable {
+    public static void main(String[] args) {
+
+        int startTemp = -20;
+
+        System.out.println("Celsius" + "\t" + "|" + "\t" + "Fahrenheit");
+
+        while (startTemp <= 30) {
+            double fahrenheit = 9.0 / 5 * startTemp + 32;
+            System.out.println(startTemp + "\t" + "|" + "\t" + fahrenheit);
+            startTemp++;
+        }
+    }
+}
---
+++ Uebungsaufgaben/Grossuebung/Temperatur.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Temperatur {
+
+    public static void main(String[] args) {
+	    Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Please enter your Celsius temperatur: ");
+        int celsius = scanner.nextInt();
+        double fahrenheit = 9.0 / 5 * celsius + 32;
+        System.out.println("The temperatur in fahrenheit is " + fahrenheit);
+
+        scanner.close();
+    }
+}
