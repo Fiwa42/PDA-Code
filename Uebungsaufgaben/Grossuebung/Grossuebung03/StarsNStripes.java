984e54e0bbb548e4308a92d211e89d0a79e1f25d
Fiwa42 <fiwa2802@protonmail.com>
2023-11-28 18:54:22 +0100

    new

---
+++ Uebungsaufgaben/Grossuebung/Grossuebung03/StarsNStripes.java
@@ -0,0 +1,17 @@
+public class StarsNStripes {
+
+	public static void main(String[] args) {
+		for (int i = 1; i < 10; i++) {
+			stars(i);
+			System.out.println();
+		}
+	}
+
+	private static void stars(int count) {
+	}
+
+	@SuppressWarnings("unused")
+	private static void stripes(int count) {
+	}
+
+}
