1db9872f50f3e2969b9cf6095d2943ac4f755cd8
Fiwa42 <fiwa2802@protonmail.com>
2023-12-21 10:08:37 +0100

    update

--- Uebungsaufgaben/UE09/SortedLinkedIntListTest.java
+++ Uebungsaufgaben/UE09/SortedLinkedIntListTest.java
@@ -7,16 +7,13 @@ public class SortedLinkedIntListTest {
     public void testSortedLinkedIntList() {
         SortedLinkedIntList sortedList = new SortedLinkedIntList();
 
-        // Test add method
         sortedList.add(5);
         sortedList.add(3);
         sortedList.add(8);
         sortedList.add(1);
 
-        // Test toArray method
         assertArrayEquals(new int[]{1, 3, 5, 8}, sortedList.toArray());
 
-        // Test reset, hasNext, and getNext methods
         sortedList.reset();
         assertTrue(sortedList.hasNext());
         assertEquals(1, sortedList.getNext());
@@ -28,11 +25,9 @@ public class SortedLinkedIntListTest {
         assertEquals(8, sortedList.getNext());
         assertFalse(sortedList.hasNext());
 
-        // Test add method after iteration
         sortedList.add(2);
         sortedList.add(7);
 
-        // Test toArray method after additional adds
         assertArrayEquals(new int[]{1, 2, 3, 5, 7, 8}, sortedList.toArray());
     }
 }
